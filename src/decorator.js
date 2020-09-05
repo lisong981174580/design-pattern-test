4081de67223363d49a42f6c91983878e5e6dd117
allen <981174580@qq.com>
2020-09-05 17:17:21 +0800

    feat: 装饰器测试完毕

--- src/decorator.js
+++ src/decorator.js
@@ -1,3 +1,48 @@
+// 装饰器模式
+
 /**
- * 装饰器模式
+ * 装饰类-混合
  */
+export function mixins(...list) {
+  return function(target) {
+    Object.assign(target.prototype, ...list);
+  }
+}
+
+/**
+ * 装饰方法-只读
+ */
+export function readonly(target, name, descriptor) {
+  // descriptor 属性描述对象(Object.defineProperty 中会用到），原来的值如下
+  // {
+  //   // 该属性对应的值。可以是任何有效的 JavaScript 值（数值，对象，函数等）,默认为 undefined。
+  //   value: specifiedFunction,
+
+  //   // 当且仅当该属性的 enumerable 键值为 true 时，该属性才会出现在对象的枚举属性中, 默认为 false
+  //   enumerable: false,
+
+  //   // 当且仅当该属性的 configurable 键值为 true 时，该属性的描述符才能够被改变，同时该属性也能从对应的对象上被删除,默认为 false。
+  //   configurable: true,
+
+  //   // 当且仅当该属性的 writable 键值为 true 时，属性的值，也就是上面的 value，才能被赋值运算符改变，默认为 false。
+  //   writable: true,
+  // }
+
+  descriptor.writable = false;
+  return descriptor;
+}
+
+/**
+ * 装饰方法-打印日志
+ */
+export function log(target, name, descriptor) {
+  const oldValue = descriptor.value;
+
+  descriptor.value = function() {
+    console.log(`Calling ${name} with`, arguments);
+
+    return oldValue.apply(this, arguments)
+  }
+
+  return descriptor;
+}
--- src/index.js
+++ src/index.js
@@ -19,3 +19,92 @@ import Target from './converter';
 const target = new Target();
 const info = target.request();
 console.log(info);
+
+// 装饰器测试，核心包 @babel/plugin-proposal-decorators
+// 原理
+// @dedecorators
+// class A {};
+
+// // 等同于
+// class A {};
+// A = dedecorators(A) || A;
+
+function testDec(target) {
+  target.isDec = true;
+}
+
+// 添加静态属性
+// @testDec 相当于 testDec(Demo)
+@testDec
+class Demo {
+
+}
+
+console.log('Demo', Demo.isDec);
+
+// 可以加参数
+function testDec2(isDec) {
+  return function(target) {
+    target.isDec = isDec;
+  }
+}
+
+@testDec2(false)
+class Demo2 {
+
+}
+
+console.log('Demo2', Demo2.isDec);
+
+
+import { mixins, readonly, log } from './decorator';
+import { deprecate } from 'core-decorators';
+
+const Foo = {
+  foo() {
+    console.log('I am foo');
+  }
+}
+
+@mixins(Foo) 
+class MyClass {
+
+}
+
+let obj = new MyClass();
+obj.foo();
+
+class Person {
+  constructor() {
+    this.first = 'A';
+    this.last = 'B';
+  }
+
+  // 装饰方法
+  @readonly
+  name() {
+    return `${this.first}-${this.last}`;
+  }
+
+  @log
+  sum(a, b) {
+    return a + b;
+  }
+
+  // @deprecate('要废弃了', { url: 'ww.baidu.com' })
+  // @deprecate('要废弃了')
+  @deprecate
+  say() {
+    console.log('say');
+  }
+}
+
+const p = new Person();
+console.log(p.name);
+
+// 以下会报错
+// p.name = function(){}
+
+const result = p.sum(1,2);
+console.log('result', result);
+p.say();
